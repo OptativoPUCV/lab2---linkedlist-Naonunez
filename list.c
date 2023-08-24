9521377a6cc73ba16100b2ebad72aefefe9cea59
Replit user <>
2023-08-24 16:24:55 +0000

    new try

--- list.c
+++ list.c
@@ -64,8 +64,8 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) { 
-  list->current=list->current->prev;
-  return list;
+
+  return list->current->data;
 }
 
 void pushFront(List * list, void * data) {
